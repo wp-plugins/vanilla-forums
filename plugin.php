6ad4590d79190103f7f4da98fe2c73f2579a5173
VanillaForums <VanillaForums@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-20 19:47:53 +0000

    tagging version 1.1.3
    git-svn-id: https://plugins.svn.wordpress.org/vanilla-forums/tags/1.1.3@507868 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: Vanilla Forums
 Plugin URI: http://vanillaforums.org/addons/
-Description: Adds deep integration for Vanilla Forums to Wordpress, including: the ability to embed the entire forum into a WordPress page; Discussion, category, and activity widgets.
-Version: 1.0.5
+Description: Integrates Vanilla Forums with Wordpress: embedded comments, embedded forum, single sign on, and Wordpress widgets.
+Version: 1.1.3
 Author: Mark O'Sullivan
 Author URI: http://www.vanillaforums.org/
 
@@ -18,6 +18,21 @@ ChangeLog:
 1.0.5
 - Fixed css & js includes so the containing folder is no longer hard-coded.
 - Added css file so Vanilla icon displays on all admin pages (not just when looking at a vanilla page).
+1.0.6
+- Forgot to add the admin.css file to svn in the last release
+1.1.0
+- Cleaned up url validation (allow you to override it now if you want)
+- Adding new jsConnect SSO functionality
+- Added comment embedding
+1.1.1
+- Somehow missed adding some files for 1.1.0
+1.1.2
+- Missing css & image files
+- Removed code that was causing embedded comments to get encoded/decoded poorly
+1.1.3
+- Fixed discussion & activity widgets to pull data correctly.
+- Removed unnecessary variables from comments.php template.
+- Removed unnecessary timestamping of count.js url.
 
 Copyright 2010 Vanilla Forums Inc
 This file is part of the Vanilla Forums plugin for WordPress.
@@ -35,5 +50,5 @@ include_once(VF_PLUGIN_PATH.'/functions.php');
 include_once(VF_PLUGIN_PATH.'/admin.php');
 include_once(VF_PLUGIN_PATH.'/embed.php');
 include_once(VF_PLUGIN_PATH.'/widgets.php');
-// include_once(VF_PLUGIN_PATH.'/sso.php');
+include_once(VF_PLUGIN_PATH.'/sso.php');
 include_once(VF_PLUGIN_PATH.'/hooks.php');
